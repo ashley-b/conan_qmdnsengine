be9dbbc5b0564464e42dcf88a130e096e95019bc
Ashley Brighthope <ashley.b@reddegrees.com>
2023-01-08 22:00:06 +1100

    feat: Added version 0.2.0-2022-01-15 with Qt6 support

--- conanfile.py
+++ conanfile.py
@@ -1,11 +1,12 @@
 from conan import ConanFile, Version
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
+from conan.tools.files.patches import apply_conandata_patches, export_conandata_patches
 from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 from conan.tools.files import copy, get, rmdir
 import os
 
-required_conan_version = ">=1.50.0"
+required_conan_version = ">=1.52.0"
 
 class QMdnsEnginConan(ConanFile):
     name = "qmdnsengine"
@@ -29,6 +30,9 @@ class QMdnsEnginConan(ConanFile):
     def layout(self):
         cmake_layout(self, src_folder="src")
 
+    def export_sources(self):
+        export_conandata_patches(self)
+
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
             check_min_cppstd(self, self._minimum_cpp_standard)
@@ -36,6 +40,9 @@ class QMdnsEnginConan(ConanFile):
         qt_version = Version(self.dependencies["qt"].ref.version)
         if qt_version < Version(self._minimum_qt_version):
             raise ConanInvalidConfiguration("{} requires qt version > {}".format(self.ref, self._minimum_qt_version))
+
+        if qt_version.major == 6 and self.version < "0.2.0-2022-01-15":
+            raise ConanInvalidConfiguration("{} requires qt 5".format(self.ref))
 
     def requirements(self):
         self.requires("qt/5.15.5")
@@ -45,6 +52,7 @@ class QMdnsEnginConan(ConanFile):
                   destination=self.source_folder, strip_root=True)
 
     def generate(self):
+        apply_conandata_patches(self)
         tc = CMakeToolchain(self)
         # Honor BUILD_SHARED_LIBS from conan_toolchain (see https://github.com/conan-io/conan/issues/11840)
         tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0077"] = "NEW"
